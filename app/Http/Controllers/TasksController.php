9ec4fac96bd065aeec31d60bb87bac23087aebfe
m.b <m.9745839.b@gmail.com>
2020-06-28 15:58:18 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -30,6 +30,11 @@ class TasksController extends Controller
    
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
@@ -61,6 +66,11 @@ class TasksController extends Controller
     
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task =Task::findOrFail($id);
         $task->status = $request->status;
         $task->content = $request->content;
